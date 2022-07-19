e5afc1ca7bc5384c624a24d6572513383c13d395
Maksym Pokulytyi <mlightpro@gmail.com>
2022-07-19 12:41:54 +0300

    back to reality

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -23,30 +23,3 @@ Statistics.propTypes = {
 };
 // dfbsdbfsdbd
 export default Statistics;
-
-
-import PropTypes from 'prop-types';
-function Statistics({ title = '1', data }) {
-  console.log(title);
-  return (
-    <section className="statistics">
-      <h2 className="title">{title && 'UPLOAD STATS'}</h2>
-      <ul className="stat-list">
-        {data.map(({ id, label, percentage }) => (
-          <li className="item" key={id}>
-            <span className="label"> {label}</span>
-            <span className="percentage"> {percentage}%</span>
-          </li>
-        ))}
-      </ul>
-    </section>
-  );
-}
-//
-Statistics.propTypes = {
-  id: PropTypes.number,
-  label: PropTypes.string,
-  percentage: PropTypes.number,
-};
-// dfbsdbfsdbd
-export default Statistics;
